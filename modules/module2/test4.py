45ee12a4bb9a5a0033a496b12c4ad987af14ab0a
Tom Van Mele <vanmelet@ethz.ch>
2019-10-21 23:01:15 +0200

    added linting setup working on ccw prep example added notes for the course

--- modules/module2/test4.py
+++ modules/module2/test4.py
@@ -1,46 +1,40 @@
+from math import radians
+
 import matplotlib.pyplot as plt
-
-from math import pi
-
-from compas.geometry import Point
-from compas.geometry import Vector
-from compas.geometry import Line
-from compas.geometry import Rotation
 from compas_plotters import Plotter2
 
+from compas.geometry import Line
+from compas.geometry import Point
+from compas.geometry import Rotation
+from compas.geometry import Vector
 
-a = Point(0, 0, 0)
-b = Point(1, 0, 0)
-c = Point(0, 1, 0)
+origin = Point(0, 0, 0)
 
-ab = Line(a, b)
-ac = Line(a, c)
+xaxis = Line(origin, origin + Vector(1.0, 0.0, 0.0))
+yaxis = Line(origin, origin + Vector(0.0, 1.0, 0.0))
 
-u = Vector(*b)
-v = Vector(*c)
+u = Vector(1.0, 0.0, 0.0)
+v = Vector(1.0, 0.0, 0.0)
 
-plotter = Plotter2(figsize=(8, 5), viewbox=[[0, 8.0], [0, 5.0]], bgcolor='#cccccc')
+plotter = Plotter2(figsize=(8, 5), viewbox=[[-2.0, 2.0], [-1.25, +1.25]])
 
-plotter.add(a)
-plotter.add(b)
-plotter.add(c)
+plotter.add(origin)
+plotter.add(xaxis)
+plotter.add(yaxis)
 
-plotter.add(ab)
-plotter.add(ac)
- 
-# plotter.add(u, point=a)
-# plotter.add(v, point=a)
+plotter.add(u)
+plotter.add(v)
 
-R = Rotation.from_axis_and_angle([0.0, 0.0, 1.0], 10 * pi / 180.0)
+R = Rotation.from_axis_and_angle([0.0, 0.0, 1.0], radians(-10))
 
 plotter.draw()
 plt.waitforbuttonpress()
 
-for i in range(9):
-    b.transform(R)
-    ab.end.transform(R)
+for i in range(18):
+    v.transform(R)
+    uxv = u.cross(v)
+    print(f"-{(i+1)*10:<4}: {uxv}")
 
-    plotter.redraw()
-    plt.waitforbuttonpress()
+    plotter.redraw(0.1)
 
 plotter.show()
