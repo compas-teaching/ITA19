8d877e20ebb46ce52ca2e24c735752ae52a6efa1
brgcode <brg@arch.ethz.ch>
2019-10-22 07:56:13 +0200

    adding a text box

--- modules/module2/test4.py
+++ modules/module2/test4.py
@@ -1,6 +1,7 @@
 from math import radians
 
 import matplotlib.pyplot as plt
+from matplotlib.text import Text
 from compas_plotters import Plotter2
 
 from compas.geometry import Line
@@ -16,25 +17,27 @@ yaxis = Line(origin, origin + Vector(0.0, 1.0, 0.0))
 u = Vector(1.0, 0.0, 0.0)
 v = Vector(1.0, 0.0, 0.0)
 
-plotter = Plotter2(figsize=(8, 5), viewbox=[[-2.0, 2.0], [-1.25, +1.25]])
+uxv = u.cross(v)
+
+plotter = Plotter2(figsize=(8, 5), viewbox=[[-1.0, 3.0], [-1, +1.50]])
 
 plotter.add(origin)
 plotter.add(xaxis)
 plotter.add(yaxis)
-
 plotter.add(u)
 plotter.add(v)
 
-R = Rotation.from_axis_and_angle([0.0, 0.0, 1.0], radians(-10))
+text = plotter.axes.add_artist(Text(x=1.0, y=0.75, text=f"Angle: 0\n{uxv}"))
 
 plotter.draw()
 plt.waitforbuttonpress()
 
+R = Rotation.from_axis_and_angle([0.0, 0.0, 1.0], radians(-10))
+
 for i in range(18):
     v.transform(R)
     uxv = u.cross(v)
-    print(f"-{(i+1)*10:<4}: {uxv}")
-
+    text.set_text(f"Angle: -{(i+1)*10:<4}\n{uxv}")
     plotter.redraw(0.1)
 
 plotter.show()
