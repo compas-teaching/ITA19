20e907cc9a731dc802254bc00b92bf3d33de278c
Tom Van Mele <vanmelet@ethz.ch>
2019-10-22 20:45:34 +0200

    moved stuff around

---
+++ modules/module2/files/crossproduct_1a.py
@@ -0,0 +1,11 @@
+# crossproduct_1a.py
+
+from compas.geometry import cross_vectors
+from compas.geometry import length_vector
+
+u = [1.0, 0.0, 0.0]
+v = [0.0, 1.0, 0.0]
+
+uxv = cross_vectors(u, v)
+
+print(length_vector(uxv))
---
+++ modules/module2/files/crossproduct_1b.py
@@ -0,0 +1,10 @@
+# crossproduct_1b.py
+
+from compas.geometry import Vector
+
+u = Vector(1.0, 0.0, 0.0)
+v = Vector(0.0, 1.0, 0.0)
+
+uxv = u.cross(v)
+
+print(uxv.length)
