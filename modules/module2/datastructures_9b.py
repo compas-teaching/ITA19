b1d33489d14e5e907e8df89e567b0b6b0e884d97
Tom Van Mele <vanmelet@ethz.ch>
2019-10-23 13:22:11 +0200

    waaaa 40 min :)

---
+++ modules/module2/datastructures_9b.py
@@ -0,0 +1,19 @@
+import os
+import compas
+
+from compas.datastructures import Mesh
+from compas.topology import breadth_first_ordering
+from compas.utilities import i_to_red
+from compas_plotters import MeshPlotter
+
+HERE = os.path.dirname(__file__)
+DATA = os.path.join(HERE, 'data')
+FILE = os.path.join(DATA, 'faces.obj')
+
+mesh = Mesh.from_obj(FILE)
+
+mesh.set_vertices_attributes('xyz', [0, 0, 0])
+
+bfo = breadth_first_ordering(mesh.adjacency, 0)
+
+print(bfo)
