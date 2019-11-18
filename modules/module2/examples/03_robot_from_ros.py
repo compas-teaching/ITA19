eee0af9673bfce924e254f82c87a015cc1620ae3
Gonzalo Casas <casas@arch.ethz.ch>
2019-11-18 23:06:46 +0100

    Simpler load robot

--- modules/module2/examples/03_robot_from_ros.py
+++ modules/module2/examples/03_robot_from_ros.py
@@ -6,13 +6,7 @@ from compas_fab.backends import RosFileServerLoader
 # Set high precision to import meshes defined in meters
 compas.PRECISION = '12f'
 
+# Load robot and its geometry
 with RosClient() as ros:
-    # Load URDF from ROS
-    loader = RosFileServerLoader(ros)
-    urdf = loader.load_urdf()
-
-    # Create robot model from URDF and load geometry
-    model = RobotModel.from_urdf_string(urdf)
-    model.load_geometry(loader)
-
-    print(model)
+    robot = ros.load_robot(load_geometry=True)
+    print(robot.model)
