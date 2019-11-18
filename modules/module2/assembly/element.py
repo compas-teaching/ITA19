e9d3122be784fbfbfa8972372d5eadfa14319cfe
Romana Rust <rust@arch.ethz.ch>
2019-11-18 20:40:02 +0100

    Update element.py

--- modules/module2/assembly/element.py
+++ modules/module2/assembly/element.py
@@ -4,7 +4,9 @@ from __future__ import division
 
 import json
 
-from compas.geometry import Frame, Transformation
+from compas.geometry import Frame
+from compas.geometry import Transformation
+from compas.datastructures import mesh_transform
 
 __all__ = ['Element']
 
@@ -19,31 +21,23 @@ class Element(object):
         The frame of the element.
     gripping_frame : :class:`compas.geometry.Frame`
         The gripping frame of the element.
-        Default is the element frame.
+        Defaults to the frame.
 
     Examples
     --------
-    from compas_fab.assembly import Element
     from compas.datastructures import Mesh
     frame = Frame.worldXY()
     element = Element(frame)
     mesh = Mesh.from_polyhedron(4)
-    element = Element.from_mesh(mesh)
     ...
     """
 
 
-    def __init__(self, frame):
+    def __init__(self, frame, mesh=None, gripping_frame=None):
         super(Element, self).__init__()
-        self._frame = None
-        self._gripping_frame = None
-
-        self.name = None
         self.frame = frame
-        self.gripping_frame = frame
-
-        self.mesh = None
-        self.centroid = frame[0]
+        self.gripping_frame = gripping_frame or frame
+        self.mesh = mesh
 
     @property
     def frame(self):
@@ -63,35 +57,32 @@ class Element(object):
     def gripping_frame(self, frame):
         self._gripping_frame = Frame(frame[0], frame[1], frame[2])
 
-    # --------------------------------------------------------------------------
-    # customisation
-    # --------------------------------------------------------------------------
-
     def __str__(self):
         """Generate a readable representation of the data of the element."""
         return str(self.data)
-
-    # --------------------------------------------------------------------------
-    # factory
-    # --------------------------------------------------------------------------
+    
+    @property
+    def centroid(self):
+        return self.mesh.centroid()
 
     @classmethod
     def from_data(cls, data):
         """Construct an element from its data representation.
+
         Parameters
         ----------
         data : :obj:`dict`
             The data dictionary.
+
         Returns
         -------
         Element
             The constructed element.
+
         Examples
         --------
-        from compas.geometry import Frame
-        from compas_fab.assembly.datastructures import Element
-        data = {frame': Frame.worldXY().data, 'gripping_frame': Frame.worldXY().data}
-        element = Element.from_data(data)
+        >>> data = {frame': Frame.worldXY().data, 'gripping_frame': Frame.worldXY().data}
+        >>> element = Element.from_data(data)
         """
         element = cls(Frame.worldXY())
         element.data = data
@@ -100,31 +91,26 @@ class Element(object):
     @property
     def data(self):
         """Returns the data dictionary that represents the element.
+
         Returns
         -------
         dict
             The element data.
+
         Examples
         --------
-        from compas.geomtry import Frame
-        from compas_fab.assembly import Element
-        element = Element(Frame.worldXY())
-        print(element.data)
-        ...
+        >>> element = Element(Frame.worldXY())
+        >>> print(element.data)
         """
-        return {'name'      : self.name,
-                'frame'     : self.frame.data,
+        return {'frame'     : self.frame.data,
                 'gripping_frame': self.gripping_frame.data,
-                'mesh'      : self.mesh,
-                'centroid'  : self.centroid
+                'mesh'      : self.mesh
                 }
 
     @data.setter
     def data(self, data):
-        self.name = data['name']
         self.frame = Frame.from_data(data['frame'])
         self.gripping_frame = data['gripping_frame']
-        self.centroid = data['centroid']
 
     def to_data(self):
         """Returns the data dictionary that represents the element.
@@ -156,31 +142,14 @@ class Element(object):
 
         centroid = mesh.centroid()
         frame_centroid = Frame(centroid, [1, 0, 0], [0, 1, 0])
-        element = Element(frame_centroid)
-        element.mesh = mesh
-
-        return element
+        return Element(frame_centroid, mesh=mesh)
     
 
     def transform(self, transformation):
+        self.frame.transform(transformation)
+        self.gripping_frame.transform(transformation)
+        mesh_transform(self.mesh, transformation)
         
-
-    # --------------------------------------------------------------------------
-    # attributes
-    # --------------------------------------------------------------------------
-
-    def centroid(self, frame):
-        """Compute the centroid of the element.
-        Returns
-        -------
-        point
-            The XYZ location of the centroid.
-        """
-        self.centroid = frame[0]
-
-        return self.centroid
-
-
     def __repr__(self):
         return 'Element({}, {})'.format(self.frame, self.gripping_frame)
 
@@ -192,7 +161,9 @@ class Element(object):
             The copy.
         """
         cls = type(self)
-        return cls(self.frame.copy, self.gripping_frame.copy)
+        return cls(self.frame.copy(), 
+                   mesh=self.mesh.copy(),
+                   gripping_frame=self.gripping_frame.copy())
 
 # ==============================================================================
 # Main
