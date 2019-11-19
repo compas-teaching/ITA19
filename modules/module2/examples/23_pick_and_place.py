e89b2551c1cfd47643b4aeaaeab1273f4966902b
Romana Rust <rust@arch.ethz.ch>
2019-11-19 18:32:44 +0100

    Update 23_pick_and_place.py

--- modules/module2/examples/23_pick_and_place.py
+++ modules/module2/examples/23_pick_and_place.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import math
+import time
 from compas.geometry import Vector
 from compas.geometry import Frame
 from compas.geometry import Box
@@ -24,15 +25,28 @@ from assembly import Element
 # define end-effector mesh and frame
 tool = Tool.from_json(os.path.join(DATA, "vacuum_gripper.json"))
 
-# create Element
+# brick dimensions
 w, l, h = 0.06, 0.03, 0.014
+
+# define start_configuration (close to picking_frame)
+start_configuration = Configuration.from_revolute_values([-5.961, 4.407, -2.265, 5.712, 1.571, -2.820])
+
+# define picking frame
+picking_frame = Frame([-0.43, 0, h], [1, 0, 0], [0, 1, 0])
+
+# define target frame
+target_frame = Frame([-0.26, -0.28, h], [1, 0, 0], [0, 1, 0])
+
+# create Element and move it at target frame
 box_frame = Frame([-w/2., -l/2, 0], [1, 0, 0], [0, 1, 0])
 box = Box(box_frame, w, l, h)
 gripping_frame = Frame([0, 0, h], [1, 0, 0], [0, 1, 0])
 element_frame = Frame([0, 0, h/2], [1, 0, 0], [0, 1, 0])
-element = Element(element_frame, 
+element = Element(element_frame,
                   mesh=Mesh.from_shape(box),
                   gripping_frame=gripping_frame)
+element.transform(Transformation.from_frame_to_frame(element.gripping_frame, target_frame))
+
 
 def acm_from_element(element, tool):
     """Transform the element into the end-effector's frame.
@@ -41,16 +55,8 @@ def acm_from_element(element, tool):
     mesh = mesh_transformed(element.mesh, T)
     return AttachedCollisionMesh(CollisionMesh(mesh, 'brick'), 'ee_link')
 
+
 brick_acm = acm_from_element(element, tool)
-
-start_configuration = Configuration.from_revolute_values([-5.961, 4.407, -2.265, 5.712, 1.571, -2.820])
-
-# define picking frame and configuration
-picking_frame = Frame([-0.43, 0, h], [1, 0, 0], [0, 1, 0])
-picking_configuration = None
-
-# define target frame
-target_frame = Frame([-0.26, -0.28, h], [1, 0, 0], [0, 1, 0])
 
 # define savelevel frames 'above' the picking- and target frames
 savelevel_vector = Vector(0, 0, 0.05)
@@ -71,35 +77,39 @@ with RosClient('localhost') as client:
 
     picking_configuration = robot.inverse_kinematics(robot.to_tool0_frame(picking_frame), start_configuration)
 
-    # 1. 'Pick' the object at the picking picking frame with a cartesian path 
-    # start_frame and start_configuration are picking_frame and picking_configuration
+    # 1. Calculate a cartesian motion from the picking frame to the savelevel_picking_frame
     frames = [picking_frame, savelevel_picking_frame]
-    frames_tool0 = robot.to_tool0_frames(frames)
 
-    trajectory1 = robot.plan_cartesian_motion(frames_tool0,
-                                              picking_configuration,
+    start_configuration = picking_configuration
+    trajectory1 = robot.plan_cartesian_motion(robot.to_tool0_frames(frames),
+                                              start_configuration,
                                               max_step=0.01,
                                               attached_collision_meshes=[brick_acm])
     assert(trajectory1.fraction == 1.)
 
-    # 2. Now calulate a free-space motion to the savelevel_target_frame
-    
-
-    # create goal constraints from frame
+    # 2. Calulate a free-space motion to the savelevel_target_frame
     goal_constraints = robot.constraints_from_frame(robot.to_tool0_frame(savelevel_target_frame),
                                                     tolerance_position,
-                                                    tolerance_axes,
-                                                    group)
+                                                    tolerance_axes)
 
-    trajectory = robot.plan_motion(goal_constraints,
-                                   start_configuration,
-                                   group,
-                                   planner_id='RRT')
+    start_configuration = trajectory1.points[-1]  # as start configuration take last trajectory's end configuration
+    trajectory2 = robot.plan_motion(goal_constraints,
+                                    start_configuration,
+                                    planner_id='RRT',
+                                    attached_collision_meshes=[brick_acm])
 
-    # scene.add_attached_collision_mesh(brick_acm)
+    # 3. Calculate a cartesian motion to the target_frame
+    frames = [savelevel_target_frame, target_frame]
 
+    start_configuration = trajectory2.points[-1]  # as start configuration take last trajectory's end configuration
+    trajectory3 = robot.plan_cartesian_motion(robot.to_tool0_frames(frames),
+                                              start_configuration,
+                                              max_step=0.01,
+                                              attached_collision_meshes=[brick_acm])
+    assert(trajectory3.fraction == 1.)
 
+    # 4. Add the brick to the planning scene
+    brick = CollisionMesh(element.mesh, 'brick_wall')
+    scene.append_collision_mesh(brick)
 
-
-    """
-    """
+    time.sleep(1)
